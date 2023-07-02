45ccbf055a4d89e5cc933559f07131d0108d4087
yuki okamura <yuki@balance.bz>
2023-07-02 18:18:36 +0900

    紙の背景を一旦

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-import './style/style.scss'
+import { PaperSvg } from "./paparsvg";
+
+const svgs = document.querySelectorAll(".block-bg-paper-svg");
+
+svgs.forEach((el) => {
+  const paperSvg = new PaperSvg(el);
+});
