05bcfc97770d6ec6a215e211eb3a29e472e39dc9
yuki okamura <yuki@balance.bz>
2023-07-02 17:16:43 +0900

    no message

--- js/margin-guide.js
+++ js/margin-guide.js
@@ -1,53 +1,53 @@
-let ele160 = document.getElementsByClassName('my--160');
-let div160 = "<div class='my border-4 border-dashed border-yellow-600 text-yellow-600 w-full flex flex-col items-center justify-center' style='height:10rem'>160px<br>10rem</div>"
-for(let i = 0; i < ele160.length; i++) {
-    ele160[i].innerHTML = div160;
-}
+// let ele160 = document.getElementsByClassName('my--160');
+// let div160 = "<div class='my border-4 border-dashed border-yellow-600 text-yellow-600 w-full flex flex-col items-center justify-center' style='height:10rem'>160px<br>10rem</div>"
+// for(let i = 0; i < ele160.length; i++) {
+//     ele160[i].innerHTML = div160;
+// }
 
-let ele80 = document.getElementsByClassName('my--80');
-let div80 = "<div class='my border-2 border-dashed border-red-500 text-red-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:5rem'>|<br>80px<br>5rem<br>|</div>"
-for(let i = 0; i < ele80.length; i++) {
-    ele80[i].innerHTML = div80;
-}
+// let ele80 = document.getElementsByClassName('my--80');
+// let div80 = "<div class='my border-2 border-dashed border-red-500 text-red-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:5rem'>|<br>80px<br>5rem<br>|</div>"
+// for(let i = 0; i < ele80.length; i++) {
+//     ele80[i].innerHTML = div80;
+// }
 
-let ele40 = document.getElementsByClassName('my--40');
-let div40 = "<div class='my border-2 border-dashed border-green-300 text-green-300 text-xs text-center flex flex-col items-center justify-center w-40' style='height:2.5rem'>40px 2.5rem</div>"
-for(let i = 0; i < ele40.length; i++) {
-    ele40[i].innerHTML = div40;
-}
+// let ele40 = document.getElementsByClassName('my--40');
+// let div40 = "<div class='my border-2 border-dashed border-green-300 text-green-300 text-xs text-center flex flex-col items-center justify-center w-40' style='height:2.5rem'>40px 2.5rem</div>"
+// for(let i = 0; i < ele40.length; i++) {
+//     ele40[i].innerHTML = div40;
+// }
 
-let ele32 = document.getElementsByClassName('my--32');
-let div32 = "<div class='my border-2 border-dashed border-green-400 text-green-400 text-xs text-center flex flex-col items-center justify-center w-40' style='height:2rem'>32px 2rem</div>"
-for(let i = 0; i < ele32.length; i++) {
-    ele32[i].innerHTML = div32;
-}
+// let ele32 = document.getElementsByClassName('my--32');
+// let div32 = "<div class='my border-2 border-dashed border-green-400 text-green-400 text-xs text-center flex flex-col items-center justify-center w-40' style='height:2rem'>32px 2rem</div>"
+// for(let i = 0; i < ele32.length; i++) {
+//     ele32[i].innerHTML = div32;
+// }
 
-let ele20 = document.getElementsByClassName('my--20');
-let div20 = "<div class='my border-2 border-dashed border-green-500 text-green-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:1.25rem'>20px 1.25rem</div>"
-for(let i = 0; i < ele20.length; i++) {
-    ele20[i].innerHTML = div20;
-}
+// let ele20 = document.getElementsByClassName('my--20');
+// let div20 = "<div class='my border-2 border-dashed border-green-500 text-green-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:1.25rem'>20px 1.25rem</div>"
+// for(let i = 0; i < ele20.length; i++) {
+//     ele20[i].innerHTML = div20;
+// }
 
-let ele16 = document.getElementsByClassName('my--16');
-let div16 = "<div class='my border-2 border-dashed border-purple-500 text-purple-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:1rem'>16px 1rem</div>"
-for(let i = 0; i < ele16.length; i++) {
-    ele16[i].innerHTML = div16;
-}
+// let ele16 = document.getElementsByClassName('my--16');
+// let div16 = "<div class='my border-2 border-dashed border-purple-500 text-purple-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:1rem'>16px 1rem</div>"
+// for(let i = 0; i < ele16.length; i++) {
+//     ele16[i].innerHTML = div16;
+// }
 
-let ele12 = document.getElementsByClassName('my--12');
-let div12 = "<div class='my border-2 border-dashed border-blue-500 text-blue-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:0.75rem'>12px 0.75rem</div>"
-for(let i = 0; i < ele12.length; i++) {
-    ele12[i].innerHTML = div12;
-}
+// let ele12 = document.getElementsByClassName('my--12');
+// let div12 = "<div class='my border-2 border-dashed border-blue-500 text-blue-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:0.75rem'>12px 0.75rem</div>"
+// for(let i = 0; i < ele12.length; i++) {
+//     ele12[i].innerHTML = div12;
+// }
 
-let ele8 = document.getElementsByClassName('my--8');
-let div8 = "<div class='my border-2 border-dashed border-sky-500 text-sky-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:0.5rem'>8px 0.5rem</div>"
-for(let i = 0; i < ele8.length; i++) {
-    ele8[i].innerHTML = div8;
-}
+// let ele8 = document.getElementsByClassName('my--8');
+// let div8 = "<div class='my border-2 border-dashed border-sky-500 text-sky-500 text-xs text-center flex flex-col items-center justify-center w-40' style='height:0.5rem'>8px 0.5rem</div>"
+// for(let i = 0; i < ele8.length; i++) {
+//     ele8[i].innerHTML = div8;
+// }
 
-let ele6 = document.getElementsByClassName('my--6');
-let div6 = "<div class='my border-2 border-dashed border-pink-500 text-pink-500 text-xs text-center flex flex-col items-center justify-center w-20' style='height:0.375rem'>6px</div>"
-for(let i = 0; i < ele6.length; i++) {
-    ele6[i].innerHTML = div6;
-}
+// let ele6 = document.getElementsByClassName('my--6');
+// let div6 = "<div class='my border-2 border-dashed border-pink-500 text-pink-500 text-xs text-center flex flex-col items-center justify-center w-20' style='height:0.375rem'>6px</div>"
+// for(let i = 0; i < ele6.length; i++) {
+//     ele6[i].innerHTML = div6;
+// }
